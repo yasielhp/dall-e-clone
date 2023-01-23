88b74c4b3ada27cdff92787a810bd0a47cd225f9
Yasiel Hernández Portal <yasielhp@gmail.com>
2023-01-23 13:16:18 +0000

    feat: :rocket: change url api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,12 +14,15 @@ export const Home = () => {
 		const fetchPosts = async () => {
 			setLoading(true)
 			try {
-				const response = await fetch('http://localhost:8080/api/v1/posts', {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				})
+				const response = await fetch(
+					'https://dall-e-wco9.onrender.com/api/v1/posts',
+					{
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					}
+				)
 				if (response.ok) {
 					const result = await response.json()
 					setAllPosts(result.data.reverse())
